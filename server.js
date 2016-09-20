d6760239bf7e2881868b357dad9f0adbfff44e8f
macbook <macbook@ychengs-MacBook-Pro.local>
2016-09-20 15:41:34 -0400

    check environment variables

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
+//require ("./test/app.js")(app);
 
 app.get('/env', function(req,res){
    res.json(process.env);
 });
 
-var ipaddress = process.env.IP;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.PORT || 3000;
 
 console.log(ipaddress);
